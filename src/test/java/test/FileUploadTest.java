893aa2ecc7c5b912c14cbe3ae108cccbc13788ef
Admin <savenokslava@gmail.com>
2024-07-31 01:36:07 +0300

    Update

--- src/test/java/test/FileUploadTest.java
+++ src/test/java/test/FileUploadTest.java
@@ -7,14 +7,16 @@ import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.io.File;
+
 public class FileUploadTest extends BaseTest {
 
     @Test
     public void fileUploadTest() {
         driver.get(ReadProperties.getUrlFileUpload());
         WebElement fileUploadElement = waitsService.presenceOfElementLocated(By.id("file-upload"));
-        String pathToFile = "D:\\Project\\AQA27\\src\\test\\resources\\data\\sample-zip-file.zip";
-        // String pathToFile = FileUploadTest.class.getClassLoader().getResource("data" + File.separator + "sample-zip-file.zip").getPath();
+        String pathToFile = FileUploadTest.class.getClassLoader().getResource("data" + File.separator + "sample-zip-file.zip").getPath().replace("/D","D").replace("%5c","/");
+        System.out.println(pathToFile);
         fileUploadElement.sendKeys(pathToFile);
         waitsService.elementToBeClickable(By.id("file-submit")).click();
         Assert.assertEquals(driver.findElement(By.id("uploaded-files")).getText(), "sample-zip-file.zip");
