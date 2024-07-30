33097e0729f469a87818c8924ee4ca3ef237992f
Admin <savenokslava@gmail.com>
2024-07-30 20:11:22 +0300

    Update

--- src/test/java/test/FileUploadTest.java
+++ src/test/java/test/FileUploadTest.java
@@ -14,7 +14,7 @@ public class FileUploadTest extends BaseTest {
         driver.get(ReadProperties.getUrlFileUpload());
         WebElement fileUploadElement = waitsService.presenceOfElementLocated(By.id("file-upload"));
         String pathToFile = "D:\\Project\\AQA27\\src\\test\\resources\\data\\sample-zip-file.zip";
-        //String pathToFile = FileUploadTest.class.getClassLoader().getResource("resources" + File.separator + "sample-zip-file.zip").getPath();
+        // String pathToFile = FileUploadTest.class.getClassLoader().getResource("data" + File.separator + "sample-zip-file.zip").getPath();
         fileUploadElement.sendKeys(pathToFile);
         waitsService.elementToBeClickable(By.id("file-submit")).click();
         Assert.assertEquals(driver.findElement(By.id("uploaded-files")).getText(), "sample-zip-file.zip");
